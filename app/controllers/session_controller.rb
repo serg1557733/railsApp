efae0d5ad7071078f8a38ee9e5e4d0c862fbc2af
Serhii Borshch <72445490+serg1557733@users.noreply.github.com>
2024-09-19 13:35:41 +0200

    fixed session controller and add coockies

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -3,5 +3,15 @@ class SessionController < ApplicationController
   end
 
   def create
+    user_params = params.require(:session).permit(:email, :password)
+    user = User.find_by(email: user_params[:email])
+
+    if user.present?
+      session[:user_id] = user.id
+      redirect_to root_path, notice: "Logged in!"
+    else
+      flash[:alert] = "Invalid email or password"
+      render :new
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
-  get 'session/new'
-  get 'session/create'
-  resources :sessions, only: [:new, :create]
+
+
   root "posts#index"
   resources :posts do
     resources :comments
   end
   resources :users, only: [:new, :create]
-
+  resources :session, only: [:new, :create]
   get "/picture", to: "posts#picture"
 
 end
